dd49ead9446187417d9201dfa01c5e3272b80329
rray524 <rray524@gmail.com>
2022-08-30 14:55:19 +0530

    add user routes & controller

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,14 @@ const PORT = process.env.PORT || 8000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/api', (req, res) => {
     res.status(200).json({ message: 'Welcome to the support ticket systems' })
 })
+
+// Login/Register Routes
+app.use('/api/users', require('../routes/userRoutes'))
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
---
+++ controllers/userController.js
@@ -0,0 +1,10 @@
+const registerUser = (req, res) => {
+    res.send('Register Users')
+}
+const loginUser = (req, res) => {
+    res.send('Login Users')
+}
+
+module.exports = {
+    registerUser, loginUser
+}
