8d65ed4e70196136b842fbfce909cf59dc0a1138
rray524 <rray524@gmail.com>
2022-08-30 13:29:10 +0530

    express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log('Server Running....')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/api', (req, res) => {
+    res.status(200).json({ message: 'Welcome to the support ticket systems' })
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
