5f9432b8a1104794dc0b2a06dfb867bb7363f127
rray524 <rray524@gmail.com>
2022-09-05 05:33:01 +0530

    cors enabled

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 require('colors')
+var cors = require('cors')
 const { errorHandler } = require('../middleware/errorHandler')
 const dotenv = require('dotenv').config()
 const connectDB = require("../config/db")
