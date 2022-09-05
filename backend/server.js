bf5cc63413ceb4b5042c0b4e0df1cd022440f350
rray524 <rray524@gmail.com>
2022-09-05 06:35:13 +0530

    issued one

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,8 @@ const dotenv = require('dotenv').config()
 const connectDB = require("../config/db")
 const PORT = process.env.PORT || 8000
 
-app.use(cors());
-
-
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
