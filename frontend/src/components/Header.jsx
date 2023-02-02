9b7d83a9587d20f82feffa70cbbfa9cb14c35a22
Rohit Mondal <rray524@gmail.com>
2023-02-02 15:33:10 +0600

    fixed

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,49 +1,48 @@
-import React from 'react';
-import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { logout, reset } from '../features/auth/authSlice'
+import React from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { logout, reset } from "../features/auth/authSlice";
 
 const Header = () => {
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-    const { user } = useSelector((state) => state.auth)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
 
-    const onLogout = () => {
-        dispatch(logout())
-        dispatch(reset())
-        navigate('/')
-    }
-    return (
-        <header className='header'>
-            <div className='logo'>
-                <Link to='/'>Ticket Support Portal</Link>
-            </div>
-            <ul>
-                {user ? (
-                    <li>
-                        <button className='btn' onClick={onLogout}>
-                            <FaSignOutAlt /> Logout
-                        </button>
-                    </li>
-                ) : (
-                    <>
-                        <li>
-                            <Link to='/login'>
-                                <FaSignInAlt /> Login
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to='/register'>
-                                <FaUser /> Register
-                            </Link>
-                        </li>
-                    </>
-                )}
-
-            </ul>
-        </header>
-    );
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/");
+  };
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Hero Ticket Support For Paid User(Programming Hero)</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
 };
 
 export default Header;
